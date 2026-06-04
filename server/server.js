0c9a2f255ab8ebb6c8b349a6bec7ab2890304f23
SujalTiwari1 <sujalaniltiwari@gmai.com>
2026-06-05 00:46:13 +0530

    feat: initialize backend with Express server and necessary dependencies
    - Added package.json to manage project dependencies and scripts
    - Created server.js to set up a basic Express server with CORS and JSON parsing
    - Implemented a simple GET endpoint that returns a greeting message

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.json(200, {
+    message: "Hello from express",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 5000");
+});
